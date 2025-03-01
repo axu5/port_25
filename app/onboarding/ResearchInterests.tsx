2478eb6c6f64a1976349060015a0505d24fdf655
axu5 <321axu123@gmail.com>
2025-03-01 19:29:20 +0200

    ui: add onboarding

---
+++ app/onboarding/ResearchInterests.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useState } from "react";
+
+export default function ResearchInterests() {
+  const [curVal, setCurVal] = useState("");
+  const [interests, setInterests] = useState<string[]>([]);
+  return (
+    <>
+      <Label>Current Interests</Label>
+      {interests.map((interest, i) => (
+        <div key={interest} className='flex flex-row gap-x-2'>
+          <Input defaultValue={interest} />
+          <Button
+            variant='destructive'
+            className='cursor-pointer'
+            onClick={() => setInterests(v => v.toSpliced(i, 1))}>
+            Delete
+          </Button>
+        </div>
+      ))}
+      <div className='flex flex-row gap-x-2'>
+        <Input
+          value={curVal}
+          onChange={e => setCurVal(e.currentTarget.value)}
+          placeholder='What are you interested in?'
+        />
+        <Button
+          type='button'
+          className='cursor-pointer'
+          variant='outline'
+          onClick={() => {
+            if (curVal.trim() === "") {
+              return;
+            }
+            setInterests(v =>
+              v.filter(x => x !== curVal).concat(curVal.trim())
+            );
+            setCurVal("");
+          }}
+          value={curVal}>
+          Add
+        </Button>
+      </div>
+    </>
+  );
+}
---
+++ app/onboarding/page.tsx
@@ -0,0 +1,45 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import ResearchInterests from "./ResearchInterests";
+
+export default async function OnboardingPage() {
+  return (
+    <div className='flex flex-col gap-y-10'>
+      <h1 className='font-semibold text-3xl'>Onboarding</h1>
+      <form className='flex flex-col gap-y-3 w-xl mx-auto'>
+        <Label htmlFor='name'>Name</Label>
+        <Input
+          name='name'
+          placeholder='What should others call you?'
+          required
+        />
+        <Label htmlFor='google-scholar'>
+          Your Google Scholar link
+        </Label>
+        <Input
+          name='google-scholar'
+          placeholder='https://scholar.google.com/...'
+          type='url'
+          required
+        />
+        <Label htmlFor='position'>Organizational position</Label>
+        <Input
+          name='position'
+          placeholder='What is your organization title?'
+          required
+        />
+        <ResearchInterests />
+        <Label htmlFor='current-work'>Current work</Label>
+        <Input
+          name='current-work'
+          placeholder='What are you working on right now?'
+          required
+        />
+        <Button type='submit' className='cursor-pointer'>
+          Start connecting!
+        </Button>
+      </form>
+    </div>
+  );
+}
